f0d1c821022f93111f1189727c9395fcaee80ec1
Ahmy Yulrizka <yulrizka@gmail.com>
2013-03-04 22:46:37 +0100

    add mail notification when light are on, add git ignore

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ import wiringpi
 import datetime
 import threading
 import time
+import sys
+import os
+from mailer import Mailer
 
-credentials = json.load(open("credentials.json"))
+fn = os.path.join(os.path.dirname(__file__), 'credentials.json')
+credentials = json.load(open(fn))
 io = None
 lightStatus = False
 
@@ -28,7 +32,6 @@ def ioSetup():
   readLightStatus()
 
 def light(on):
-  print "{}: lights {}".format(datetime.datetime.now(), "On" if on else "Off")
   if on:
     status = io.HIGH
     lightStatus = True
@@ -37,6 +40,7 @@ def light(on):
     lightStatus = False
 
   io.digitalWrite(18, status)
+  sys.stdout.write("{}: lights {}\n".format(datetime.datetime.now(), "On" if on else "Off"))
 
 def isLate():
   now = datetime.datetime.now()
@@ -51,7 +55,7 @@ def lightsOffWhenIsLate():
   while True:
     time.sleep(60)
     if isLate() and readLightStatus() == 1:
-      print "It's late, turning off lights"
+      sys.stdout.write("It's late, turning off lights\n")
       light(False)
 
 #define our own condition
@@ -61,29 +65,35 @@ def onAfterOfficeHour(date, value):
   weekday = dateTime.weekday()
   hour = dateTime.hour
   afterOfficeHour =  weekday in range(0,5) and hour >= 17
-  print "after office {} result: {}".format(dateTime, afterOfficeHour)
+  sys.stdout.write ("after office {} result: {}\n".format(dateTime, afterOfficeHour))
   return value if afterOfficeHour else None
 
 @eventExpression("onNotLate")
 def onNotLate(date, value):
-  return not isLate()
+  return None if isLate() else True
 
 @eventExpression("turnOnLight")
 def turnOnLight(date, value):
   light(True)
+  message = "lights is on at {}".format(datetime.datetime.now())
+  user = credentials["email"]["username"]
+  password = credentials["email"]["password"]
+  mailer = Mailer(user, password)
+  mailer.send("ahmy@sense-os.nl", "Light is on", message)
 
 def main():
     ioSetup()
 
+    # just turn off the light when it is lage (> 21.00)
+    th = threading.Thread(target=lightsOffWhenIsLate)
+    #th.daemon = True
+    th.start()
+
     #light(True)
     senseHQ = Position(address="Lloydstraat 5, Rotterdam, Netherlands")
     putri = User("putri129@gmail.com", credentials["putri129@gmail.com"])
-    putri.event().sensor("position", ownedBy="ahmy@sense-os.nl").departedFrom(senseHQ).onAfterOfficeHour().onNotLate().turnOnLight().realTime(60).makeItSo()
+    putri.event().sensor("position", ownedBy="ahmy@sense-os.nl").departedFrom(senseHQ).onAfterOfficeHour().onNotLate().turnOnLight().realTime(60).run()
 
-    # just turn off the light when it is lage (> 21.00)
-    th = threading.Thread(target=lightsOffWhenIsLate)
-    th.daemon = True
-    th.start()
 
 if __name__ == '__main__':
     main()
